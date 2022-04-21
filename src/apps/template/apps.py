ebb3ec2359e103b778e34e4267f2e95359573f8e
Pere <pere@perepicornell.com>
2022-04-21 15:11:39 +0200

    Black formatting

--- src/apps/template/apps.py
+++ src/apps/template/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class TemplateConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.template'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.template"
