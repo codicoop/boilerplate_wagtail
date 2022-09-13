eccd713e38419880516f0773c1f4f1555951cdcd
Pere Picornell <pere@perepicornell.com>
2022-09-13 16:19:47 +0200

    Afegit camp main_section_image

--- src/apps/cms_site/models/custom_projects.py
+++ src/apps/cms_site/models/custom_projects.py
@@ -1,3 +1,4 @@
+from django.apps import apps
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from wagtail.admin.panels import MultiFieldPanel
@@ -12,11 +13,32 @@ class CustomProjectsPage(MenuLabelMixin, BasePage):
     parent_page_types = ["cms_site.HomePage"]
     page_description = _("Main page for custom projects section.")
     max_count = 1
-    template = "pages/custom_projects_page.html"
+    template = "pages/custom_projects/page.html"
+
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(request, *args, **kwargs)
+        collection_page_model = apps.get_model("cms_site", "CustomProject")
+        collection_pages = collection_page_model.objects.live()
+        context.update(
+            {
+                "custom_projects": collection_pages,
+            }
+        )
+        return context
 
 
 class CustomProject(BasePage):
     description = models.TextField(_("Description"), default="", blank=True)
+    main_section_image = models.ForeignKey(
+        "wagtailimages.Image",
+        verbose_name=_("Main section image"),
+        on_delete=models.PROTECT,
+        related_name="+",
+        null=False,
+        blank=False,
+        help_text="This image will be used in the Custom Projects section, "
+        "when displaying the list of projects."
+    )
     images = StreamField(
         [
             ("image", ImageChooserBlock()),
@@ -26,6 +48,7 @@ class CustomProject(BasePage):
 
     content_panels = BasePage.content_panels + [
         FieldPanel("description", classname="full"),
+        FieldPanel("main_section_image"),
         MultiFieldPanel(
             [
                 FieldPanel("images"),
@@ -36,5 +59,5 @@ class CustomProject(BasePage):
 
     parent_page_types = ["cms_site.CustomProjectsPage"]
     page_description = _("Custom project page.")
-    template = "pages/custom_project.html"
+    template = "pages/custom_projects/custom_project.html"
     show_in_menus_default = False
