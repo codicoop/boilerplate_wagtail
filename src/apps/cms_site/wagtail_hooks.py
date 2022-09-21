93613513768f3e33d64aa216023899bb480b656b
Pere Picornell <pere@perepicornell.com>
2022-09-21 16:37:08 +0200

    A l'eliminar un projecte a mida, a demés de la col·lecció també s'eliminen les imatges.

--- src/apps/cms_site/wagtail_hooks.py
+++ src/apps/cms_site/wagtail_hooks.py
@@ -1,4 +1,5 @@
 from wagtail import hooks
+from wagtail.images.models import Image
 from wagtail.models import Collection
 
 from apps.cms_site.models import CustomProject
@@ -13,4 +14,5 @@ def after_delete_custom_project(request, page):
         and page.specific_class is CustomProject
         and page.images_collection
     ):
+        Image.objects.filter(collection=page.images_collection).delete()
         Collection.objects.get(id=page.images_collection.id).delete()
