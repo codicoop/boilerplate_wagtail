43e08534c7d5573de6d168d952f9cbe2660f1436
Pere Picornell <pere@perepicornell.com>
2022-09-21 12:14:47 +0200

    Afegit hook que elimina la col·lecció quan s'elimina un projecte a mida.

---
+++ src/apps/cms_site/wagtail_hooks.py
@@ -0,0 +1,16 @@
+from wagtail import hooks
+from wagtail.models import Collection
+
+from apps.cms_site.models import CustomProject
+
+
+@hooks.register("after_delete_page")
+def after_delete_custom_project(request, page):
+    """Delete CustomProject's linked Collection."""
+
+    if (
+        request.method == 'POST'
+        and page.specific_class is CustomProject
+        and page.images_collection
+    ):
+        Collection.objects.get(id=page.images_collection.id).delete()
