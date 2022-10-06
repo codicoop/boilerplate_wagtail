ae45a9bb1e559be264434cf267924ab58ce86ecb
MartaRF3 <ricartfm@gmail.com>
2022-10-06 10:26:47 +0200

    Fixed error in hsla color format

--- src/assets/js/react/FilterComponent/FilterApp.js
+++ src/assets/js/react/FilterComponent/FilterApp.js
@@ -1,24 +1,30 @@
-import React from 'react';
+import React, { useeState, useState } from 'react';
 
 export default function FilterApp(){
+  const [filters, setFilters] = useState(initialFilters)
   const filterTitleLabel = "Filtra els models"
   const filterTypeLabel = "Tipus"
   const filterModelLabel = "Model"
   const filterSubmodelLabel = "Submodel / Acabat"
+  const initialFilters = {
+    type: "",
+    model: "",
+    submodel: ""
+  }
 
   return(
-    <section class="collections-detail__filters">
-      <form action="" class="grid-1">
-        <div class="collections-detail__filters-title title-3 grid-item-1-3">
+    <section className="collections-detail__filters">
+      <form action="" className="grid-1">
+        <div className="collections-detail__filters-title title-3 grid-item-1-3">
           <p>{filterTitleLabel}</p>
         </div>
-        <div class="collections-detail__filters-select btn btn--dark grid-item-4-6">
+        <div className="collections-detail__filters-select btn btn--dark grid-item-4-6">
           <p>{filterTypeLabel}</p>
         </div>
-        <div class="collections-detail__filters-select btn btn--dark grid-item-7-9">
+        <div className="collections-detail__filters-select btn btn--dark grid-item-7-9">
           <p>{filterModelLabel}</p>
         </div>
-        <div class="collections-detail__filters-select btn btn--dark grid-item-10-12">
+        <div className="collections-detail__filters-select btn btn--dark grid-item-10-12">
           <p>{filterSubmodelLabel}</p>
         </div>
       </form>
