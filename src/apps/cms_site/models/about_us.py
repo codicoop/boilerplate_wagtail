b07ea10f657549aa725cbc536e801693101abd7e
Pere Picornell <pere@perepicornell.com>
2023-02-22 15:49:47 +0100

    Afegits 6 camps per les descripcions dels vídeos.

--- src/apps/cms_site/models/about_us.py
+++ src/apps/cms_site/models/about_us.py
@@ -1,3 +1,4 @@
+from django.db import models
 from wagtail.admin.panels import FieldPanel
 from wagtail.fields import StreamField
 from django.utils.translation import gettext_lazy as _
@@ -16,8 +17,44 @@ class AboutUsPage(BasePage):
         null=True,
         blank=True,
     )
+    video_description_1 = models.TextField(
+        _("Video 1 description"),
+        default="",
+        blank=True,
+    )
+    video_description_2 = models.TextField(
+        _("Video 2 description"),
+        default="",
+        blank=True,
+    )
+    video_description_3 = models.TextField(
+        _("Video 3 description"),
+        default="",
+        blank=True,
+    )
+    video_description_4 = models.TextField(
+        _("Video 4 description"),
+        default="",
+        blank=True,
+    )
+    video_description_5 = models.TextField(
+        _("Video 5 description"),
+        default="",
+        blank=True,
+    )
+    video_description_6 = models.TextField(
+        _("Video 6 description"),
+        default="",
+        blank=True,
+    )
 
     content_panels = BasePage.content_panels + [
+        FieldPanel("video_description_1"),
+        FieldPanel("video_description_2"),
+        FieldPanel("video_description_3"),
+        FieldPanel("video_description_4"),
+        FieldPanel("video_description_5"),
+        FieldPanel("video_description_6"),
         FieldPanel("history_items_list"),
     ]
 
