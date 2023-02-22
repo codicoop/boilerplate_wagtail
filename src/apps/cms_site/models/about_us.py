c9bc01012d358cb4fd611e5086aa5054f9cd361a
Pere Picornell <pere@perepicornell.com>
2023-02-22 16:28:56 +0100

    elements de la història del qui som passats a un model en comptes d'streamfield

--- src/apps/cms_site/models/about_us.py
+++ src/apps/cms_site/models/about_us.py
@@ -27,7 +27,11 @@ class AboutUsPage(BasePage):
             heading=_("Videos"),
             label=_("Video"),
         ),
-        FieldPanel("history_items_list"),
+        InlinePanel(
+            "history_items",
+            heading=_("History"),
+            label=_("History item"),
+        ),
     ]
 
     template = "pages/about_us.html"
@@ -49,6 +53,33 @@ class VideoItem(Orderable, ClusterableModel):
         FieldPanel("title"),
         FieldPanel("embed"),
         FieldPanel("description"),
+    ]
+
+    def __str__(self):
+        return self.title
+
+
+class HistoryItem(Orderable, ClusterableModel):
+    page = ParentalKey(
+        AboutUsPage,
+        on_delete=models.CASCADE,
+        related_name="history_items",
+    )
+    year = models.IntegerField(_("Year"))
+    title = models.CharField(_("Title"), max_length=80)
+    description = models.TextField(_("Description"))
+    image = models.ForeignKey(
+        "wagtailimages.Image",
+        verbose_name=_("Image"),
+        on_delete=models.CASCADE,
+        related_name="+",
+    )
+
+    panels = [
+        FieldPanel("year"),
+        FieldPanel("title"),
+        FieldPanel("description"),
+        FieldPanel("image"),
     ]
 
     def __str__(self):
