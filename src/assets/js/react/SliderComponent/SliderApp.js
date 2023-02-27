bc435aa58dc27ff037b07e4aceba00cb45901ee6
MartaRF3 <ricartfm@gmail.com>
2023-02-27 11:48:10 +0100

    #89 Dades de historia venen de wagtail

--- src/assets/js/react/SliderComponent/SliderApp.js
+++ src/assets/js/react/SliderComponent/SliderApp.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useTranslation } from "react-i18next"
+import axios from 'axios';
 import './i18n';
 
 export default function SliderApp(){
