37fca7193c02cd9e1b5c38967c0e455cda4e3888
Pere Picornell <pere@perepicornell.com>
2023-03-01 17:40:29 +0100

    Eliminat endpoint pels posts d'instagram

--- src/apps/cms_api/urls.py
+++ src/apps/cms_api/urls.py
@@ -3,7 +3,6 @@ from rest_framework.routers import DefaultRouter
 from apps.cms_api.views import (
     CollectionItemViewSet,
     HistoryItemViewSet,
-    InstagramPostViewSet,
     VideoItemViewSet,
 )
 
@@ -22,11 +21,6 @@ router.register(
     r"history_items",
     HistoryItemViewSet,
     basename="history_item",
-)
-router.register(
-    r"instagram_posts",
-    InstagramPostViewSet,
-    basename="instagram_post",
 )
 
 urlpatterns = router.urls
