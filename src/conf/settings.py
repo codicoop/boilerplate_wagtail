d0549fc6dac5fec47bd316573fb0570c3061b2f8
Pere Picornell <pere@perepicornell.com>
2023-05-25 16:17:01 +0200

    afegit setting LOCALE_PATHS

--- src/conf/settings.py
+++ src/conf/settings.py
@@ -221,6 +221,9 @@ WAGTAIL_CONTENT_LANGUAGES = LANGUAGES = [
     ("es", "Spanish"),
 ]
 WAGTAIL_I18N_ENABLED = True
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, 'locale'),
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
