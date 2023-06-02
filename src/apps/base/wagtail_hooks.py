8f1f58d711b1627a17566e5ce1d62055c3c2d83e
Pere Picornell <pere@perepicornell.com>
2023-06-02 09:21:27 +0200

    Fix: creació de pàgines noves

--- src/apps/base/wagtail_hooks.py
+++ src/apps/base/wagtail_hooks.py
@@ -1,15 +1,6 @@
 from wagtail import hooks
 
 
-@hooks.register('construct_page_action_menu')
-def remove_disabled_actions(menu_items, request, context):
-    specific_page = context["page"].specific
-    if not specific_page.is_submitable:
-        menu_items[:] = [item for item in menu_items if item.name != 'action-submit']
-    if not specific_page.is_unpublishable:
-        menu_items[:] = [item for item in menu_items if item.name != 'action-unpublish']
-
-
 @hooks.register('construct_page_listing_buttons')
 def remove_page_listing_button_item(buttons, page, page_perms, context=None):
     specific_page = page.specific
