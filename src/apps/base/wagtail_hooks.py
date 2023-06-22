c107331bc9f8768ee8083936d8ae2c137f6eb5e4
Pere Picornell <pere@perepicornell.com>
2023-06-22 13:00:51 +0200

    Hook comentat degut a que donava error i ara no és important limitar les opcions dels desplegables.

--- src/apps/base/wagtail_hooks.py
+++ src/apps/base/wagtail_hooks.py
@@ -1,8 +1,8 @@
 from wagtail import hooks
 
 
-@hooks.register("construct_page_listing_buttons")
-def remove_page_listing_button_item(buttons, page, page_perms, context=None):
-    specific_page = page.specific
-    if not specific_page.show_more_dropdown_in_list_actions:
-        buttons.pop()  # removes the last button, which is the 'more' dropdown.
+# @hooks.register("construct_page_listing_buttons")
+# def remove_page_listing_button_item(buttons, page, page_perms, context=None):
+#     specific_page = page.specific
+#     if not specific_page.show_more_dropdown_in_list_actions:
+#         buttons.pop()  # removes the last button, which is the 'more' dropdown.
